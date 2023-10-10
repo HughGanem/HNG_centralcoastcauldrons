85d70a3fb9795ed5559bbfd2ae81c7a2c40de2cd
Hugh Ganem <hganem@calpoly.edu>
2023-10-10 10:07:33 -0700

    Catalog change 10/10/23

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,26 +19,35 @@ def get_catalog():
         number_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory;")).first().num_green_potions
         number_blue_potions = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory;")).first().num_blue_potions
 
-    return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": number_red_potions,
-                "price": 1,
-                "potion_type": [100, 0, 0, 0],
-            },
-            {
-                "sku": "GREEN_POTION_0",
-                "name": "green potion",
-                "quantity": number_green_potions,
-                "price": 1,
-                "potion_type": [0, 100, 0, 0],
-            },
-            {
-                "sku": "BLUE_POTION_0",
-                "name": "blue potion",
-                "quantity": number_blue_potions,
-                "price": 1,
-                "potion_type": [0, 0, 100, 0],
-            }
-        ]
+    return_lst = []
+
+    if (number_red_potions != 0):
+        return_lst.append(
+        {
+            "sku": "RED_POTION_0",
+            "name": "red potion",
+            "quantity": number_red_potions,
+            "price": 1,
+            "potion_type": [100, 0, 0, 0],
+        })
+    
+    if (number_green_potions != 0):
+        return_lst.append(
+        {
+            "sku": "GREEN_POTION_0",
+            "name": "green potion",
+            "quantity": number_green_potions,
+            "price": 1,
+            "potion_type": [0, 100, 0, 0],
+        })
+    
+    if (number_blue_potions != 0):
+        return_lst.append(
+        {
+            "sku": "BLUE_POTION_0",
+            "name": "blue potion",
+            "quantity": number_blue_potions,
+            "price": 1,
+            "potion_type": [0, 0, 100, 0],
+        })
+    return return_lst
